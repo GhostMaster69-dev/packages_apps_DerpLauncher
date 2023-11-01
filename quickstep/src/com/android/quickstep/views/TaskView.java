36284cd2da4973c975e542368a2a8e7a8d64a275
MrSluffy <werdna.jac@gmail.com>
2023-11-01 12:00:44 +0800

    DerpLauncher: Introducing Task Lock in Task Menu
    In situations where users frequently run the app in the background, and open multiple apps, there may be instances where a specific app should not be cleared when performing a 'Clear All' or 'Dismiss' action. This new feature ensures that a selected running task or app remains in the Recent View and will not be removed.

--- quickstep/src/com/android/quickstep/views/TaskView.java
+++ quickstep/src/com/android/quickstep/views/TaskView.java
@@ -65,6 +65,7 @@ import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.IntDef;
@@ -349,6 +350,7 @@ public class TaskView extends FrameLayout implements Reusable {
     protected Task mTask;
     protected TaskThumbnailView mSnapshotView;
     protected IconView mIconView;
+    private ImageView mLockedView;
     protected final DigitalWellBeingToast mDigitalWellBeingToast;
     protected float mFullscreenProgress;
     private float mGridProgress;
@@ -501,6 +503,7 @@ public class TaskView extends FrameLayout implements Reusable {
         super.onFinishInflate();
         mSnapshotView = findViewById(R.id.snapshot);
         mIconView = findViewById(R.id.icon);
+        mLockedView = findViewById (R.id.dis_lock);
         mIconTouchDelegate = new TransformingTouchDelegate(mIconView);
     }
 
@@ -575,6 +578,7 @@ public class TaskView extends FrameLayout implements Reusable {
     public void bind(Task task, RecentsOrientedState orientedState) {
         cancelPendingLoadTasks();
         mTask = task;
+        updateLockedView (task.isLocked);
         mTaskIdContainer[0] = mTask.key.id;
         mTaskIdAttributeContainer[0] = new TaskIdAttributeContainer(task, mSnapshotView,
                 mIconView, STAGE_POSITION_UNDEFINED);
@@ -1026,6 +1030,7 @@ public class TaskView extends FrameLayout implements Reusable {
                         (task) -> {
                             setIcon(mIconView, task.icon);
                             mDigitalWellBeingToast.initialize(task);
+                            updateLockedView(task.isLocked);
                         });
             }
         } else {
@@ -1639,6 +1644,8 @@ public class TaskView extends FrameLayout implements Reusable {
         progress = Utilities.boundToRange(progress, 0, 1);
         mFullscreenProgress = progress;
         mIconView.setVisibility(progress < 1 ? VISIBLE : INVISIBLE);
+        boolean taskLockState = TaskUtils.getTaskLockState(getContext(), this.mTask.key.baseIntent.getComponent(), mTask.key);
+        mLockedView.setVisibility(taskLockState ? VISIBLE : INVISIBLE);
         mSnapshotView.getTaskOverlay().setFullscreenProgress(progress);
 
         // Animate icons and DWB banners in/out, except in QuickSwitch state, when tiles are
@@ -1723,6 +1730,27 @@ public class TaskView extends FrameLayout implements Reusable {
             params.height = expectedHeight;
             setLayoutParams(params);
         }
+    }
+
+    public void updateLockedView(boolean isLock, boolean isState) {
+        if (this.mLockedView == null) {
+            Log.d(TAG, "updateLockedView: mLockedView is null.");
+            return;
+        }
+        Task task = mTask;
+        if (!(task == null || task.key == null || !isState)) {
+            if (isLock == (this.mLockedView.getVisibility() != View.VISIBLE)) {
+                boolean taskLockState = TaskUtils.getTaskLockState(getContext(), mTask.key.baseIntent.getComponent(), mTask.key);
+                Log.d(TAG, "updateLockedView: update task lockState: " + isState + " -> " + taskLockState + " , task.key.id: " + mTask.key.id);
+                mTask.isLocked = taskLockState;
+                isLock = taskLockState;
+            }
+        }
+        mLockedView.setVisibility(isLock ? VISIBLE : INVISIBLE);
+    }
+
+    public void updateLockedView(boolean isLock) {
+        updateLockedView(isLock, true);
     }
 
     private float getGridTrans(float endTranslation) {
